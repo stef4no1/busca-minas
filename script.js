c9dac7a001b28323d225c3f30ea9db9f72dbb86f
Stefano Straga <stefanostraga2k18@gmail.com>
2022-11-08 00:20:50 +0100

    upload js

--- script.js
+++ script.js
@@ -81,10 +81,64 @@ function clickTile() {
         return;
     }
 
-
     let coords = tile.id.split("-"); 
     let r = parseInt(coords[0]);
     let c = parseInt(coords[1]);
     checkMine(r, c);
+}
+
+function revealMines() {
+    for (let r= 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+            let tile = board[r][c];
+            if (minesLocation.includes(tile.id)) {
+                tile.innerText = "💣";
+                tile.style.backgroundColor = "red";                
+            }
+        }
+    }
+}
+
+function checkMine(r, c) {
+    if (r < 0 || r >= rows || c < 0 || c >= columns) {
+        return;
+    }
+    if (board[r][c].classList.contains("tile-clicked")) {
+        return;
+    }
+
+    board[r][c].classList.add("tile-clicked");
+    tilesClicked += 1;
+
+    let minesFound = 0;
+
+    minesFound += checkTile(r-1, c-1);      
+    minesFound += checkTile(r-1, c);        
+    minesFound += checkTile(r-1, c+1);
+    minesFound += checkTile(r, c-1);    
+    minesFound += checkTile(r, c+1);   
+    minesFound += checkTile(r+1, c-1);    
+    minesFound += checkTile(r+1, c);      
+    minesFound += checkTile(r+1, c+1);      
+
+    if (minesFound > 0) {
+        board[r][c].innerText = minesFound;
+        board[r][c].classList.add("x" + minesFound.toString());
+    }
+    else {
+        checkMine(r-1, c-1);   
+        checkMine(r-1, c);     
+        checkMine(r-1, c+1);
+        checkMine(r, c-1);  
+        checkMine(r, c+1); 
+        checkMine(r+1, c-1);    
+        checkMine(r+1, c);      
+        checkMine(r+1, c+1);    
+    }
+
+    if (tilesClicked == rows * columns - minesCount) {
+        document.getElementById("mines-count").innerText = "Cleared";
+        gameOver = true;
+    }
 
 }
